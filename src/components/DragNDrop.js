24de243aac48cc085f70b69d97b1d107e83f35e3
Василь Камалов <vasilkamalov@FromKazanWithLove.local>
2021-11-19 20:14:04 +0300

    working but not testing well

--- src/components/DragNDrop.js
+++ src/components/DragNDrop.js
@@ -13,6 +13,7 @@ import "../styles/app.css"
 function DragNDrop() {
     const [taskList, setTaskList] = useState([])
     const [inputChanged, setInputChanged] = useState("")
+    const [isFalse, setIsFalse] = useState(false);
 
     var tasksTodo = {
         wip: [],
@@ -23,56 +24,69 @@ function DragNDrop() {
     const usersCollectionRef = collection(db, "todolist");
 
     const onDragStart = (ev, name, id) => {
-        ev.dataTransfer.setData("name", name);
         ev.dataTransfer.setData("id", id);
+        console.log("onDragStart")
     }
 
     const onDragOver = async (ev) => {
         ev.preventDefault();
+        console.log("onDragOver")
     }
 
+
     const onDrop = async (ev, cat) => {
-        let name = ev.dataTransfer.getData("name");
         let id = ev.dataTransfer.getData("id");
 
         taskList.map((task) => {
-            if (task.name === name) {
+            if (task.id === id) {
                 task.category = cat
             }
             return task;
         });
+        // const newListId = taskList.filter(item => item.id === id)
+        // setTaskList({
+        //     category: cat
+        // })
+        // console.log(newListId)
 
+
+        setTaskList([...taskList]);
+        console.log("onDrop")
+        //Меняет в БД значение id перемещаемого элемента
         const listDoc = doc(db, "todolist", id);
         await updateDoc(listDoc, {
             category: cat
         });
 
-        setTaskList([...taskList]);
     }
+
+
     //Добавляет новую задачу в список через State и fFirestore
     const handlerAdd = async () => {
-        let tempTaskList = taskList.concat({
-            name: inputChanged,
-            category: "wip",
-            id: new Date(),
-        })
-
-        setTaskList([...tempTaskList]) //Записывает новые значения из инпута в стайт
-        console.log(tempTaskList)
+        // let tempTaskList = taskList.concat({
+        //     name: inputChanged,
+        //     category: "wip",
+        //     id: new Date(),
+        // })
+        //
+        // setTaskList([...tempTaskList]) //Записывает новые значения из инпута в стайт
+        // console.log(taskList)
 
         //  Запись нового task из инпута в лист firestore
+
         await addDoc(usersCollectionRef, {
             name: inputChanged,
             category: "wip",
             id: new Date(),
         })
+        setIsFalse(true)
     }
 
+
     const deleteTask = async (id) => {
-        // const newDeleteList = taskList.filter(item => item.id !== id)
-        // setTaskList(newDeleteList);
         const userDoc = doc(db, "todolist", id);
         await deleteDoc(userDoc);
+        setIsFalse(true)
         console.log(taskList)
     }
 
@@ -83,6 +97,7 @@ function DragNDrop() {
                 onDragStart={(e) => onDragStart(e, t.name, t.id)}
                 draggable
                 // style = {{backgroundColor: t.bgcolor}}
+                className="box-for-task"
             >
                 {t.name}
                 <button className="tasksButton" onClick={() => {
@@ -94,10 +109,23 @@ function DragNDrop() {
         );
     });
 
+
+// -------------Запись в локальный стэйт из Firestore-------------
     const getData = async () => {
         const data = await getDocs(usersCollectionRef)
         setTaskList(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
     };
+
+    useEffect(() => {
+        if (isFalse) {
+            console.log("useEffectInner");
+            getData();
+        }
+        console.log("useEffectOuter")
+        setIsFalse(false)
+    }, [tasksTodo]);
+//END -------------Запись в локальный стэйт из Firestore-------------
+
 
     //  Не работает по нажатию Ентер
 
@@ -113,9 +141,6 @@ function DragNDrop() {
     //     };
     // }, []);
 
-    useEffect(() => {
-        getData();
-    }, [tasksTodo]);
 
     return (
         <div className="wrapper-draggable">
@@ -125,7 +150,7 @@ function DragNDrop() {
 
                     setInputChanged(event.target.value)
                 }}
-                placeholder="Нужно сделать.."
+                       placeholder="Нужно сделать.."
                 />
                 <button onClick={handlerAdd}>submit</button>
             </div>
@@ -133,28 +158,28 @@ function DragNDrop() {
             <div className="container-drag">
                 <h2 className="header">TASKS DRAG AND DROP LIST</h2>
                 <div className="container-drag-block">
-                <div className="wip"
-                     onDragOver={(e) => onDragOver(e, "wip")}
-                     onDrop={(e) => {
-                         onDrop(e, "wip")
-                     }}>
-                    <span className="task-header">Что нужно сделать</span>
-                    {tasksTodo.wip}
-                </div>
+                    <div className="wip"
+                         onDragOver={(e) => onDragOver(e, "wip")}
+                         onDrop={(e) => {
+                             onDrop(e, "wip")
+                         }}>
+                        <span className="task-header">Что нужно сделать</span>
+                        {tasksTodo.wip}
+                    </div>
 
-                <div className="droppable"
-                     onDragOver={(e) => onDragOver(e, "complete")}
-                     onDrop={(e) => onDrop(e, "complete")}>
-                    <span className="task-header">В разработке</span>
-                    {tasksTodo.complete}
-                </div>
+                    <div className="droppable"
+                         onDragOver={(e) => onDragOver(e, "complete")}
+                         onDrop={(e) => onDrop(e, "complete")}>
+                        <span className="task-header">В разработке</span>
+                        {tasksTodo.complete}
+                    </div>
 
-                <div className="droppable03"
-                     onDragOver={(e) => onDragOver(e, "WhatTodo")}
-                     onDrop={(e) => onDrop(e, "WhatTodo")}>
-                    <span className="task-header">Сделано</span>
-                    {tasksTodo.WhatTodo}
-                </div>
+                    <div className="droppable03"
+                         onDragOver={(e) => onDragOver(e, "WhatTodo")}
+                         onDrop={(e) => onDrop(e, "WhatTodo")}>
+                        <span className="task-header">Сделано</span>
+                        {tasksTodo.WhatTodo}
+                    </div>
                 </div>
             </div>
         </div>
