3d7f0187ab34e6562b57c91f9c889624fc5593ef
Vasil Kamalov <77620048+SerjioValentes@users.noreply.github.com>
2021-11-29 10:21:45 +0300

    update firebase data

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -5,12 +5,7 @@ import { getFirestore } from "@firebase/firestore";
 
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDqAOQXKO_U1tWJ1L5xc0vt8u9xhbBY2Nk",
-    authDomain: "dndtodolist.firebaseapp.com",
-    projectId: "dndtodolist",
-    storageBucket: "dndtodolist.appspot.com",
-    messagingSenderId: "260034265711",
-    appId: "1:260034265711:web:805a6cff33563716de039a"
+*
 }
 
 const fire = firebase.initializeApp(firebaseConfig)
